862541f95e6f7dbec34cb54e403ecabf109f2705
arfinaleusuf <arfinaleusuf@gmail.com>
2026-09-21 22:51:43 +0600

    rider router

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ app.add_middleware(
 models.Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(admin.router)
+app.include_router(rider.router)
 
 def get_db():
     db = SessionLocal()
